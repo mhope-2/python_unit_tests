cd4616c8e8ad53922b2a28389af68713727f800a
mhope <mhope@npontu.com>
2021-07-26 17:58:16 +0000

    initial setup

---
+++ arithmetic.py
@@ -0,0 +1,27 @@
+
+def add(num1, num2):
+
+    """ add function """
+    return num1 + num2
+
+
+def subtract(num1, num2):
+
+    """ subtract function """
+    return num1 - num2
+
+def multiply(num1, num2):
+
+    """ multiply function """
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """ divide function """
+    if num2 == 0:
+        raise ValueError('Can not divide by zero!')
+    return num1 / num2
+
+
+    
+    
---
+++ readme.md
@@ -0,0 +1,4 @@
+### Python Unit Test Practice
+```
+Ref: https://www.youtube.com/watch?v=6tNS--WetLI
+```
