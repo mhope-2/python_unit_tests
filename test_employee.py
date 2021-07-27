658f945a65104487417c5a9db25358da1b30b5a8
mhope <mhope@npontu.com>
2021-07-27 03:59:13 +0000

    refactor(): added employee and employee test

--- test_employee.py
+++ test_employee.py
@@ -3,6 +3,8 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
     def test_email(self):
+        """ Test employee emails """
+
         emp1 = Employee(first_name='Michael', last_name='Hope', pay=1000000)
         emp2 = Employee(first_name='John', last_name='Hagan', pay=2500)
 
@@ -10,10 +12,20 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(emp2.email, 'John.Hagan@email.com')
 
         emp1.first_name = 'Larry'
-        emp1.last_name = 'Jay'
+        emp2.first_name = 'Jay'
 
-        self.assertEqual(emp1.email, 'Larry.Jay@email.com')
+        self.assertEqual(emp1.email, 'Larry.Hope@email.com')
+        self.assertEqual(emp1.email, 'Jay.Hagan@email.com')
 
+
+    def test_full_name(self):
+        """ Test employee full name """
+
+        emp1 = Employee(first_name='Jay', last_name='Smith', pay=1000000)
+        emp2 = Employee(first_name='Say', last_name='Mannor', pay=12500)
+
+        self.assertEqual(emp1.full_name, 'Jay Smith')
+        self.assertEqual(emp2.full_name, 'Say Mannor')
 
 
 
