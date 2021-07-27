67364e321ca131acf3b5e40abe0db400a1fc0058
mhope <mhope@npontu.com>
2021-07-27 03:15:51 +0000

    refactor(): refactored test_arithmetic.py

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -4,13 +4,19 @@ import arithmetic
 
 class TestArithmetic(unittest.TestCase):
     def test_add(self):
-        """ sample edge cases """
+        """ sample addition edge cases """
         self.assertEqual(arithmetic.add(10, 5), 15)
         self.assertEqual(arithmetic.add(-1, 1), 0)
         self.assertEqual(arithmetic.add(-1, -1), -2)
 
         with self.assertRaises(TypeError):
             arithmetic.add(1, '2')
+
+
+    def test_subtract(self):
+        """ sample subtraction edge cases """
+        self.assertEqual(arithmetic.subtract())
+
 
 if __name__ == '__main__':
     unittest.main()
