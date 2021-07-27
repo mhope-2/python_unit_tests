6f54b624bca3415fcdff1c9950dc8ca61d6c73e9
mhope <mhope@npontu.com>
2021-07-27 03:19:35 +0000

    refactor(): refactored test_arithmetic.py

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -15,7 +15,26 @@ class TestArithmetic(unittest.TestCase):
 
     def test_subtract(self):
         """ sample subtraction edge cases """
-        self.assertEqual(arithmetic.subtract())
+        self.assertEqual(arithmetic.subtract(12, 4), 8)
+        self.assertEqual(arithmetic.add(10, 5), 5)
+        self.assertEqual(arithmetic.add(-1, 1), -2)
+        self.assertEqual(arithmetic.add(-1, -1), 2)
+
+
+    def test_multiply(self):
+        """ sample addition edge cases """
+        self.assertEqual(arithmetic.multiply(10, 5), 50)
+        self.assertEqual(arithmetic.multiply(-1, 1), -1)
+        self.assertEqual(arithmetic.multiply(-1, -1), 1)
+        self.assertEqual(arithmetic.subtract(12, 4), 48)
+
+
+    def test_divide(self):
+        """ sample addition edge cases """
+        self.assertEqual(arithmetic.divide(10, 5), 2)
+        self.assertEqual(arithmetic.divide(-1, 1), -1)
+        self.assertEqual(arithmetic.divide(-1, -1), 1)
+        self.assertEqual(arithmetic.subtract(12, 4), 3)
 
 
 if __name__ == '__main__':
